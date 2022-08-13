5f19dfb33a02e28ce9cd8c3c5c9258fdccd39520
Alex Kurov <Alex>
2022-08-13 18:13:19 +0300

    docs(routes): add page list

---
+++ src/router/routes.ts
@@ -0,0 +1,11 @@
+export enum routes {
+    NOT_FOUND = '*',
+    HOME ='/',
+    SEARCH = '/search',
+    BOOK = '/book',
+    CART = '/cart',
+    FAVORITES = '/favorites',
+    ACCOUNT = '/account',
+    SIGNIN = '/sigh-in',
+    SIGNUP = '/sign-up',
+}
