1bb39677eeb9c3cf0a2fa025580c4085ccd9c411
Alex Kurov <twistter8@gmail.com>
2022-08-22 14:42:18 +0300

    feat(main-pages) add loader with library

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import { StyledLoading } from './style'
+import { CSSProperties } from "react";
+import { RingLoader } from "react-spinners";
+import { useNewBooks } from "../../hooks/useNewBooks";
+
+const override: CSSProperties = {
+  display: "block",
+  margin: "100px auto 0 auto",
+};
 
 export const Loading = () => {
+  const { loading } = useNewBooks();
+
   return (
     <>
-        <StyledLoading>Loading...</StyledLoading>
+      <RingLoader loading={loading} cssOverride={override} size={50} />
     </>
-  )
-}
+  );
+};
