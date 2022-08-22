bfdfc8a0b4ba47be3341a58e33fdd96dfa5f6b06
Alex Kurov <Alex>
2022-08-22 22:50:26 +0300

    feat(home-pages): add subscribe form on the home pages

---
+++ src/components/Subscribe/style.ts
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+import { Colors } from "../../ui/colors";
+import { fonts } from "../../ui/typography";
+import { StyledButton } from "../Button/style";
+
+export const StyledSubscribe = styled.div`
+  margin-left: 64px;
+  margin-right: 64px;
+  padding-bottom: 56px;
+`;
+
+export const Title = styled.h2`
+  padding-top: 56px;
+  ${fonts.H2}
+`;
+export const Subtitle = styled.p`
+  ${fonts.Subline}
+  font-weight: 400;
+  color: ${Colors.Secondary};
+  padding-top: 10px;
+`;
+export const Input = styled.input`
+  display: inline-block;
+  flex: 0 1 80%;
+  border: none;
+  padding: 20px 0px 20px 20px;
+  margin: 0;
+  outline: none;
+`;
+export const Button = styled(StyledButton)`
+  display: inline-block;
+  margin: 0;
+  padding-left: 15px;
+  padding-right: 15px;
+  cursor: pointer;
+`;
+export const Background = styled.div`
+  background: ${Colors.Purple};
+  margin-top: 56px;
+`;
+export const InputContainer = styled.form`
+  display: flex;
+  padding-top: 32px;
+`;
