bcafdf058e769db3f944d27e921d20271c80d812
Alex Kurov <Alex>
2022-08-25 23:27:18 +0300

    refactor(fix): add new styles for component BookItem

--- src/components/BookItem/style.ts
+++ src/components/BookItem/style.ts
@@ -2,16 +2,19 @@ import styled from "styled-components";
 import { Color, Space, fonts } from "../../ui";
 
 export const StyledBookItem = styled.div`
-  width: 352px;
+  width: 280px;
   margin-bottom: 50px;
+  
 `;
 export const ItemBackground = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   background-color: ${Color.Blue};
+  height: 320px;
   border-radius: 10px;
   margin-bottom: ${Space.Small};
+  box-shadow: 10px  10px 5px${Color.Orange};
 `;
 
 export const Image = styled.img`
@@ -34,4 +37,11 @@ export const SubTitle = styled.p`
     margin-bottom: ${Space.Large};
   flex-grow: 1;
 `;
-export const Cost = styled(Title)``;
+export const Cost = styled(Title)`
+display: flex;
+align-items: flex-end;
+align-self: flex-start;
+flex-grow: 1;
+padding-top: 10px;
+padding-left: 10px;
+`;
