17317397053a4ffdf719688190bb782a92f45c55
Alex Kurov <twistter8@gmail.com>
2022-08-26 11:44:29 +0300

    feat(rating): add new component Rating

---
+++ src/components/Raiting/Raiting.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { StyledRating } from './style'
+import { FaStar  } from 'react-icons/fa'
+import { BsStar  } from 'react-icons/bs'
+
+interface IProps {
+    rating: string
+}
+
+export const Raiting = ({rating}: IProps) => {
+
+    const currRating = +rating
+    const totalRating = 5
+
+  return (
+    <StyledRating>
+            {[...new Array(totalRating)].map((arr, index) => {
+                if (index < currRating) {
+                    return <FaStar key={index} style={{ fontSize: '24px', color: '#ffd700' }} />
+                } else return <BsStar key={index} style={{ fontSize: '24px' }} />
+            })}
+        </StyledRating>
+  )
+}
---
+++ src/components/Raiting/index.ts
@@ -0,0 +1,3 @@
+import { Raiting } from "./Raiting";
+
+export { Raiting };
