f03102235d723f55b7f4af4e68ef8939f6cb6410
Alex Kurov <Alex>
2022-08-31 23:03:00 +0300

    feat(account): add store and user slices rx-toolkit

---
+++ src/store/index.ts
@@ -0,0 +1,5 @@
+import { removeUser, setUser } from "./slices/userSlice";
+import { AppDispatch, RootState } from "./store";
+
+export { removeUser, setUser };
+export type { AppDispatch, RootState };
