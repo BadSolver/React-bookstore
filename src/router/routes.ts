cd3c79a55a6aff486265e8c4d77c53fdc1409929
Alex Kurov <twistter8@gmail.com>
2022-09-01 11:35:48 +0300

    refactor(account): edit path to resetPassword

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -10,5 +10,5 @@ export enum ROUTE {
   // SIGNUP = "/sign-up",
   HELLO_PAGE = "/react-bookstore",
   REGISTRATION = "registration",
-  RESET_PASSWORD = "/sign-in/ResetPassword",
+  RESET_PASSWORD = "/sign-in/reset-password",
 }
