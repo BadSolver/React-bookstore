a9058af28fcb03aab7d73443b1d8fec60829dd27
Alex Kurov <twistter8@gmail.com>
2022-09-01 11:33:35 +0300

    refactor(account): change spelling of keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,11 @@
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_Auth_Domain,
-  projectId: process.env.REACT_APP_Project_Id,
-  storageBucket: process.env.REACT_APP_Storage_Bucket,
-  messagingSenderId: process.env.REACT_APP_Messaging_Sender_Id,
-  appId: process.env.REACT_APP_App_Id,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SSENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 // eslint-disable-next-line no-unused-vars
 export const app = initializeApp(firebaseConfig);
