7159dac298137b16f3aea5bff1692b963290143f
Alex Kurov <Alex>
2022-09-04 15:35:13 +0300

    refactor(redux): rewrite the slider component under redux

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,45 +1,23 @@
-import React, { CSSProperties, useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { StyledSlider } from "./style";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { AxiosError } from "axios";
-import { bookStoreAPI } from "../../services/bookStoreApi";
-import { INewBooksApi } from "../../types";
 import { BookItem } from "../BookItem";
-import { RingLoader } from "react-spinners";
-import { ErrorMessage } from "../ErrorMessage";
+import {
+  useAppDispatch,
+  useAppSelector,
+} from "../../store/redux-hooks/redux-hooks";
+import { getNewBooks } from "../../store/selectors";
+import { fetchNewBooks } from "../../store/slices/newBooksSlice";
 
 export const SliderCom = () => {
-  const [books, setBooks] = useState<INewBooksApi[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string>("");
-
-  const fetchBooks = async () => {
-    try {
-      setError("");
-      setIsLoading(true);
-
-      await bookStoreAPI.getNew().then((book) => {
-        setBooks(book.books);
-      });
-
-      setIsLoading(false);
-    } catch (e) {
-      const error = e as AxiosError;
-      setIsLoading(false);
-      setError(error.message);
-    }
-  };
+  const { books } = useAppSelector(getNewBooks);
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
-    fetchBooks();
-  }, []);
-
-  const override: CSSProperties = {
-    display: "block",
-    margin: "100px auto 0 auto",
-  };
+    dispatch(fetchNewBooks());
+  }, [dispatch]);
 
   const settings = {
     dots: true,
@@ -80,11 +58,9 @@ export const SliderCom = () => {
   return (
     <StyledSlider>
       <Slider {...settings}>
-          {isLoading && <RingLoader cssOverride={override} size={50} />}
-          {error && <ErrorMessage text={error} />}
-          {books.map((book) => {
-            return <BookItem book={book} key={book.isbn13} />;
-          })}
+        {books.map((book) => {
+          return <BookItem book={book} key={book.isbn13} />;
+        })}
       </Slider>
     </StyledSlider>
   );
