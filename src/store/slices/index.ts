15ee15b9186482c0fc0dade8fde8d5e926442890
Alex Kurov <twistter8@gmail.com>
2022-09-07 18:46:37 +0300

    refactor(search): update barrel pattern

--- src/store/slices/index.ts
+++ src/store/slices/index.ts
@@ -1,4 +1,32 @@
 import { fetchDetailsBook } from "./bookDetailsSlice";
+import {
+  addItem,
+  removeItem,
+  clearCart,
+  countPlus,
+  countMinus,
+} from "./cartSlice";
 import { fetchNewBooks } from "./newBooksSlice";
+import {
+  removeSearchValue,
+  setSearchValue,
+  incrementPage,
+  decrementPage,
+} from "./searchSlice";
+import { setUser, removeUser } from "./userSlice";
 
-export { fetchDetailsBook, fetchNewBooks };
+export {
+  fetchDetailsBook,
+  fetchNewBooks,
+  addItem,
+  removeItem,
+  clearCart,
+  countPlus,
+  countMinus,
+  removeSearchValue,
+  setSearchValue,
+  incrementPage,
+  decrementPage,
+  setUser,
+  removeUser,
+};
