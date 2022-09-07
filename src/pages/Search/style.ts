32db4aa0049d5893dc873336bfcbccc867fe2321
Alex Kurov <twistter8@gmail.com>
2022-09-07 18:48:24 +0300

    feat(search): add styles for search page and component

--- src/pages/Search/style.ts
+++ src/pages/Search/style.ts
@@ -1,1 +1,41 @@
-export {};
+import styled from "styled-components";
+import { Color } from "../../ui";
+
+export const StyledSearch = styled.section``;
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10%;
+`;
+
+export const Previous = styled.button`
+  margin-top: 30px;
+  font-size: 21px;
+  color: ${Color.Secondary};
+  background-color: #fff;
+  border: none;
+  &:hover {
+    color: ${Color.Primary};
+  }
+`;
+
+export const Next = styled.button`
+  margin-top: 30px;
+  font-size: 21px;
+  color: ${Color.Secondary};
+  background-color: #fff;
+  border: none;
+  &:hover {
+    color: ${Color.PrimaryDark};
+  }
+`;
+
+export const PrevText = styled.span`
+  margin-left: 30px;
+`;
+
+export const NextText = styled.span`
+  margin-right: 30px;
+`;
