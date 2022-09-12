c7f5b4ef083d1b4264a915781028a691860aeb8f
Alex Kurov <twistter8@gmail.com>
2022-09-13 01:01:55 +0300

    refactor(dev): update slider media resolution

--- src/components/Slider/style.ts
+++ src/components/Slider/style.ts
@@ -2,6 +2,9 @@ import styled from "styled-components";
 import { Color } from "../../ui";
 
 export const StyledSlider = styled.div`
+@media (max-width: 800px) {
+  max-width: 330px;
+}
   .slick-arrow.slick-prev {
     z-index: 100;
   }
