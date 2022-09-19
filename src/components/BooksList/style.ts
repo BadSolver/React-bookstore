3e33e6793e2ee27ab471c44c46e3bbd81626ce1d
Alex Kurov <twistter8@gmail.com>
2022-09-19 17:04:22 +0300

    refactor(fix): edit BookItem and BookList

--- src/components/BooksList/style.ts
+++ src/components/BooksList/style.ts
@@ -5,6 +5,7 @@ export const StyledBooksList = styled(motion.ul)`
   display: grid;
   grid-template-columns: auto auto auto auto;
   border-radius: 10px;
+  z-index: 999;
   
   @media (max-width: 1220px) {
     grid-template-columns: auto auto auto;
